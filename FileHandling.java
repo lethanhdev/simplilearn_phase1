95c4f7daa6f442721821b3cdf63aa319f8b240a6
Thanh Le <36800795+lethanhdev@users.noreply.github.com>
2020-12-02 14:08:51 -0800

    Add files via upload

---
+++ FileHandling.java
@@ -0,0 +1,122 @@
+package simplilearn;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class FileHandling {
+
+	final File f = new File("/Users/thanhle/simplilearn/phase1/selfLearning/section6/src/simplilearn/_test4.txt");
+	static Scanner input = new Scanner(System.in);
+	
+	boolean exit;
+
+	public static void main(String[] args) throws IOException {
+		FileHandling menu = new FileHandling();
+		menu.runMenu();
+	}
+
+	public void runMenu() throws IOException {
+		printHeader();
+		while (!exit) {
+			printMenu();
+			int choice = getMenuChoice();
+			performAction(choice);
+		}
+		input.close();
+	}
+
+	private void printHeader() {
+		System.out.println("+---------------------------+");
+		System.out.println("|  Menu: File Handling      |");
+		System.out.println("+---------------------------+");
+	}
+
+	private void printMenu() {
+		System.out.println();
+		System.out.println("1) Create a new File");
+		System.out.println("2) Read to File");
+		System.out.println("3) Write to File");
+		System.out.println("4) Append to File");
+		System.out.println("0) Exit");
+	}
+
+	private int getMenuChoice() {
+		int choice = -1;
+		do {
+			System.out.print("\nEnter your choice: ");
+			try {
+				choice = Integer.parseInt(input.nextLine());
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid selection. Numbers only please.");
+			}
+			if (choice < 0 || choice > 4) {
+				System.out.println("Choice outside of range. Please chose again.");
+			}
+		} while (choice < 0 || choice > 4);
+		//keyboard.close();
+		return choice;
+	}
+	
+	private static void createFile(File file) throws IOException {
+		try {
+			if (file.createNewFile()) {
+				System.out.println("File created: " + file.getName() + "\n");
+			} else {
+				System.out.println("File already exists.\n");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.\n");
+			e.printStackTrace();
+		}
+	}
+	
+	private static void readFile(File file) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String str;
+		while ((str = br.readLine()) != null) 
+			System.out.println(str);
+		br.close();
+	}
+	
+	private static void writeFile(File file) throws IOException {		
+	    try {
+	        FileWriter myWriter = new FileWriter(file);
+	        System.out.println("Enter your text to write to file: ");
+	        myWriter.write(input.nextLine());
+	        myWriter.close();
+	        System.out.println("Successfully wrote to the file.");
+	      } catch (IOException e) {
+	        System.out.println("An error occurred.");
+	        e.printStackTrace();
+	      }
+	}
+	
+	private static void appendFile(File file) throws IOException {	
+		FileWriter writer = new FileWriter(file, true);
+		System.out.print("append your text here: ");
+		writer.write(input.nextLine());
+		//sc.close();
+		writer.close();
+		
+	}
+
+	private void performAction(int choice) throws IOException {
+		switch (choice) {
+		case 0:
+			System.out.println("Good Bye.");
+			System.exit(0);
+			break;
+		case 1: createFile(f); break;
+		case 2: readFile(f); break;
+		case 3: writeFile(f); break;
+		case 4: appendFile(f); break;
+		default: System.out.println("\nUnknown error has occured.\n");
+		}
+	}
+
+}//FileHandling
