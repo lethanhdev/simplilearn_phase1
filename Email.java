6ccc61f343fbe2e0ba2b7f710c28efcd5205c344
Thanh Le <36800795+lethanhdev@users.noreply.github.com>
2020-12-02 20:13:21 -0800

    Update Email.java

--- Email.java
+++ Email.java
@@ -1,6 +1,7 @@
 package simplilearn;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,24 +11,30 @@ public class Email {
 		
 		String searchId = "username@yahoo.corporate.in";		//user's search email defined
 		
-		ArrayList<String> inArr = new ArrayList<>(); 	//create a list of email
+		ArrayList<String> inArr = new ArrayList<>(); 			//create a list of email
 		inArr.add("user@domain.com");
 		inArr.add("user@domain.co.in");
 		inArr.add("user.name@domain.com");
 		inArr.add("user_name@domain.com");
 		inArr.add("username@yahoo.corporate.in");
-			
-		for (String str: inArr)							//print out email list
-			System.out.println(str);		
-		System.out.println("-------");
 		
-		if (searchEmail(inArr, searchId) != null) { 		//search and validate email
-			System.out.print("found: " + searchId);
+		System.out.println("Array of strings:");
+		for (String str: inArr)									//print out email list
+			System.out.println(str);	
+		
+		System.out.println("\n> Please enter an email id: ");	//get input from user
+		Scanner input = new Scanner(System.in);
+		searchId = input.nextLine();
+		System.out.println("\n------- searching for email ...");				
+		
+		if (searchEmail(inArr, searchId) != null) { 			//search and validate email
+			System.out.print("\nfound: " + searchId);
 			if (validate(searchId)) 
 				System.out.println(", valid");
 			else
 				System.out.println("not valid");
-		}	
+		}			
+		input.close();
 	}//main()	
 
 	public static String searchEmail(ArrayList<String> strArr, String id) {
